56e5dd4c1d20f10acf5b3b735aa5b7d6c2756506
totosmurra <82951283+totosmurra@users.noreply.github.com>
2021-05-05 15:23:30 -0300

    Update ejercicio_3.py

--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -35,3 +35,59 @@ puntaje = 70
 
 # Debe imprimir en pantalla la calificacion
 # Utilizar "if" anidados
+
+
+
+'''
+# Condicionales anidados
+numero_1 = 7
+numero_2 = -2
+
+# Verifique si el numero_1 es mayor a 5
+#   --> En caso afirmativo, verifique si el numero_2
+#       es positivo
+#       --> En caso afirmativo imprima en pantalla "Resp=1"
+#       --> En caso negativo imprima en pantalla   "Resp=2"
+#  --> En caso negativo (numero_1 no es mayor a 5)
+#      verifique si el numero_2 es mayor a 5
+#       --> En caso afirmativo imprima en pantalla "Resp=3"
+#       --> En caso negativo imprima en pantalla "Resp=4"
+'''
+
+if numero_1 > 5:
+    if numero_2 > 0:
+        print("resp1")
+    else:
+        print("resp2")
+else:
+    if numero_2 > 5:
+        print("resp3")
+    else:
+        print("resp4")
+
+'''
+# Verifique la calificación de un estudiante según su
+# puntaje en un examen
+puntaje = 70
+
+# Si el puntaje es mayor igual a 90 --> imprimir A
+# Si el puntaje es mayor igual a 80 --> imprimir B
+# Si el puntaje es mayor igual a 70 --> imprimir C
+# Si el puntaje es mayor igual a 60 --> imprimir D
+# Si el puntaje es menor a  60      --> imprimir F
+
+# Debe imprimir en pantalla la calificacion
+# Utilizar "if" anidados
+'''
+
+if puntaje >= 90:
+    print ("A")
+elif 90>puntaje>=80:
+    print ("B")
+elif 80>puntaje>=70:
+    print ("C")
+elif 70>puntaje>=60:
+    print ("D")
+elif puntaje<60:
+    print ("F")
+
