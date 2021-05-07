0a6cea89fa42b505b2b69fc248853ae0f7a501e4
totosmurra <82951283+totosmurra@users.noreply.github.com>
2021-05-07 15:11:52 -0300

    Update profundizacion_4.py

--- ejercicios_profunidzacion/profundizacion_4.py
+++ ejercicios_profunidzacion/profundizacion_4.py
@@ -29,3 +29,45 @@ e imprimir en pantalla de la mayor a la menor
 
 print('Ejercicios de práctica con cadenas')
 # Empezar aquí la resolución del ejercicio
+
+print ("palabra 1")
+palabra1 = str(input())
+
+
+
+print ("palabra 2")
+palabra2 = str(input())
+
+
+
+print ("palabra 3")
+palabra3 = str(input())
+
+
+
+
+
+print ("operacion")
+operacion = str(input())
+
+
+
+palabras = [palabra1, palabra2, palabra3]
+
+
+if operacion == "1":
+    palabras.sort()
+    print (palabras)
+else:
+    lenpal1 = int(len(palabra1))
+    lenpal2 = int(len(palabra2))
+    lenpal3 = int(len(palabra3))
+    
+    lenpals = [lenpal1, lenpal2, lenpal3]
+    lenpals.sort()
+    print (lenpals)
+
+    
+
+
+
