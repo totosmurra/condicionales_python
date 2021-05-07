81926bbbf5c0ffdbe316efb8210507e16e6e1755
totosmurra <82951283+totosmurra@users.noreply.github.com>
2021-05-07 14:25:59 -0300

    Update profundizacion_2.py

--- ejercicios_profunidzacion/profundizacion_2.py
+++ ejercicios_profunidzacion/profundizacion_2.py
@@ -23,3 +23,39 @@ Para cada caso imprimir el resultado en pantalla.
 
 print('Ejercicios de práctica con números')
 # Empezar aquí la resolución del ejercicio
+
+
+nmr1 = int(input())
+nmr2 = int(input())
+nmr3 = int(input())
+
+
+
+nmr1f = nmr1 % 2
+nmr2f = nmr2 % 2
+nmr3f = nmr3 % 2
+
+
+
+print (nmr1f)
+print (nmr2f)
+print (nmr3f)
+
+
+
+if nmr1f == 1:
+    print ("El primer numero es impar")
+else:
+    print ("El primer numero es par")
+
+if nmr2f == 1:
+    print ("El segundo numero es impar")
+else:
+    print ("El segundo numero es par")
+
+if nmr3f == 1:
+    print ("El tercer numero es impar")
+else:
+    print ("El tercer numero es par")
+
+
