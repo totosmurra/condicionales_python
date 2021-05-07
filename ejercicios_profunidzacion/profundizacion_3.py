27478c8a04afca5556841352584f77b06421e3f7
totosmurra <82951283+totosmurra@users.noreply.github.com>
2021-05-07 14:47:33 -0300

    Update profundizacion_3.py

--- ejercicios_profunidzacion/profundizacion_3.py
+++ ejercicios_profunidzacion/profundizacion_3.py
@@ -26,3 +26,36 @@ En cada caso imprimir en pantalla el resultado
 
 print('Ejercicios de práctica con números')
 # Empezar aquí la resolución del ejercicio
+
+
+
+temp1 = int(input())
+temp2 = int(input())
+temp3 = int(input())
+
+
+
+# 1 - ¿Cuáles de ellas es la máxima temperatura ingresada?
+if temp1 > temp2 and temp1 > temp3:
+    print ("La primera temperatura es mayor")
+elif temp2 > temp1 and temp2 > temp3:
+    print ("La segunda temperatura es mayor")
+else:
+    print ("La tercera temperatura es mayor")
+
+
+
+
+# 2 - ¿Cuáles de ellas es la mínima temperatura ingresada?
+if temp1 < temp2 and temp1 < temp3:
+    print ("La primera temperatura es menor")
+elif temp2 < temp1 and temp2 < temp3:
+    print ("La segunda temperatura es menor")
+else:
+    print ("La tercera temperatura es menor")
+
+
+
+# 3 - ¿Cuál es el promedio de las temperaturas ingresadas?
+tempprom = (temp1 + temp2 + temp3)/3
+print (tempprom)
